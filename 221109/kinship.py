9696092785c2627177cd69879ee3cc2eed39470e
sanghoy <shyoon@8pin.co.kr>
2022-11-14 14:22:39 +0900

    study

--- 221109/kinship.py
+++ 221109/kinship.py
@@ -7,24 +7,25 @@ result = []
 def kinship():
     people = int(input("전체 사람의 수를 입력하세요 : "))
     while True:
-        a, b = int(input("비교 대상을 입력하세요 : ").split())
+        a, b = map(int, input("비교 대상을 입력하세요 : ").split())
         if 1 <= a <= people and 1 <= b <= people:
             break
         print("정확히 입력하세요.")
     count = int(input("관계의 수를 입력하세요 : "))
+    kinship = [[] for _ in range(count)]
     for _ in range(count):
         while True:
-            x, y = input("관계를 입력하세요 : ").split()
+            x, y = map(int, input("관계를 입력하세요 : ").split())
             if 1 <= x <= people and 1 <= y <= people and x != y:
+                kinship[x].append(y)
+                kinship[y].append(x)
                 break
             print("정확히 입력하세요.")
-        kinship[x].append(y)
-        kinship[y].append(x)
     search(a, 0)
     if len(result) == 0:
         print(-1)
     else:
-        print(result[0])
+        print(result[0] - 1)
 
 def search(a, cnt):
     cnt += 1
