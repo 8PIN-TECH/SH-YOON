6ba1ba9bd9afa73b8665cd7583bd7f841ae84cd8
sanghoy <shyoon@8pin.co.kr>
2022-11-14 17:01:46 +0900

    study

--- 221109/kinship.py
+++ 221109/kinship.py
@@ -1,41 +1,37 @@
-people  = 0
-a       = 0
-b       = 0
-count   = 0
-result  = []
+result  = [0]
+people = int(input("전체 사람의 수를 입력하세요 : "))
+signal = [0] * (people + 1)
+while True:
+    a, b = map(int, input("비교 대상을 입력하세요 : ").split())
+    if 1 <= a <= people and 1 <= b <= people:
+        break
+    print("정확히 입력하세요.")
+count = int(input("관계의 수를 입력하세요 : "))
+child = [[] for _ in range(people + 1)]
+for _ in range(count):
+    while True:
+        x, y = map(int, input("관계를 입력하세요 : ").split())
+        if 1 <= x <= people and 1 <= y <= people and x != y:
+            child[x].append(y)
+            child[y].append(x)
+            break
+        print("정확히 입력하세요.")
 
 def kinship():
-    input()
     search(a, 0)
     if len(result) == 0:
         print(-1)
     else:
         print(result[0] - 1)
 
-def input():
-    people = int(input("전체 사람의 수를 입력하세요 : "))
-    while True:
-        a, b = map(int, input("비교 대상을 입력하세요 : ").split())
-        if 1 <= a <= people and 1 <= b <= people:
-            break
-        print("정확히 입력하세요.")
-    count = int(input("관계의 수를 입력하세요 : "))
-    kinship = [[] for _ in range(count)]
-    for _ in range(count):
-        while True:
-            x, y = map(int, input("관계를 입력하세요 : ").split())
-            if 1 <= x <= people and 1 <= y <= people and x != y:
-                kinship[x].append(y)
-                kinship[y].append(x)
-                break
-            print("정확히 입력하세요.")
-
-def search(a, cnt):
+def search(v, cnt):
     cnt += 1
-    if a == b:
+    signal[v] = 1
+    if v == b:
         result[0] = cnt
-    for i in kinship[a]:
-        search(i, cnt)
+    for i in child[v]:
+        if signal[i] != 1:
+            search(i, cnt)
 
 if __name__ == '__main__':
     kinship()
