056bfba70143fae362e2d2da7ab80b9205063728
sanghoy <shyoon@8pin.co.kr>
2022-11-21 12:59:31 +0900

    study_1121

--- 221116/maze.py
+++ 221116/maze.py
@@ -1,10 +1,33 @@
-def maze():
-    print("test")
+from collections import deque
+# 시계방향
+dx = deque([0, 1, 0, -1])
+dy = deque([-1, 0, 1, 0])
+N, M = map(int, input("미로의 크기를 입력하세요. : ").split())
+maze = []
+for _ in range(N):
+    maze.append(list(map(int, input())))
+# 미로 모양보기
+# for i in maze:
+#     print(i)
+
+def BFS(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+
+        for _ in range(4):
+            nx = x + dx[0]
+            ny = y + dy[0]
+            dx.rotate(-1)
+            dy.rotate(-1)
+
+            if (0 <= nx < N and 0 <= ny < M):
+                if (maze[nx][ny] == 1):
+                    queue.append((nx, ny))
+                    maze[nx][ny] = maze[x][y] + 1
 
 if __name__ == '__main__':
-    x, y = map(int, input("미로의 크기를 입력하세요. : ").split())
-    road = []
-    for _ in range(x):
-        road.append(list(map(int,input())))
-    print(road[2][2])
-    maze()
+    BFS(0, 0)
+    print(maze[N - 1][M - 1])
